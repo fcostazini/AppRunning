d8c1a730f3fc215fbd3ed9fbcfdec34930f95bf2
fcostazini <fcostazini@conexia.com>
2015-05-20 17:32:59 -0300

    Agrego Google +

--- Android/AppRunning/mobile/src/main/java/studios/thinkup/com/apprunning/BuscarCarreraActivity.java
+++ Android/AppRunning/mobile/src/main/java/studios/thinkup/com/apprunning/BuscarCarreraActivity.java
@@ -1,25 +1,23 @@
 package studios.thinkup.com.apprunning;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 
-public class BuscarCarreraActivity extends ActionBarActivity {
+public class BuscarCarreraActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_carrera);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
+            getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
