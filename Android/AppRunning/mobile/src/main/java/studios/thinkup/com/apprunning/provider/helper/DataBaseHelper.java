855258df68065f569a9f6504cf4299cfe7ade511
FaQ <fcostazini@gmail.com>
2015-08-13 00:27:43 -0300

    cambio de version de db

--- Android/AppRunning/mobile/src/main/java/studios/thinkup/com/apprunning/provider/helper/DataBaseHelper.java
+++ Android/AppRunning/mobile/src/main/java/studios/thinkup/com/apprunning/provider/helper/DataBaseHelper.java
@@ -20,7 +20,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     //The Android's default system path of your application database.
 
     private final static String DATABASE_NAME = "runningDB.sqlite";
-    private final static int DATABASE_VERSION = 397;
+    private final static int DATABASE_VERSION = 398;
 
 
     private SQLiteDatabase myDataBase;
