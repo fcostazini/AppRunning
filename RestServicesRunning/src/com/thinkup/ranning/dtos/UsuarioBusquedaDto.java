e09c18a10e82ec32075bfbc0ee67405b608fc5e6
FaQ <fcostazini@gmail.com>
2015-09-19 14:41:18 -0300

    agrego dto de busqueda*

---
+++ RestServicesRunning/src/com/thinkup/ranning/dtos/UsuarioBusquedaDto.java
@@ -0,0 +1,42 @@
+package com.thinkup.ranning.dtos;
+
+public class UsuarioBusquedaDto {
+	private String nombre;
+	private String apellido;
+	private String nickName;
+	private String email;
+	private String grupo;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGrupo() {
+		return grupo;
+	}
+	public void setGrupo(String grupo) {
+		this.grupo = grupo;
+	}
+	
+	
+
+}
